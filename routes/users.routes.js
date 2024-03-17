0be714e5f09c62407ba989bc353d90ad3136d616
christiangubana <christiangubana@gmail.com>
2024-03-17 23:05:47 +0200

    create user routers

---
+++ routes/users.routes.js
@@ -0,0 +1,10 @@
+const userController = require("../controllers/user.controllers");
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get("/user-profile", userController.userProfile);
+
+module.exports = router;
