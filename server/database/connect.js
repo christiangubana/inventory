6613e31d644fa228aaa9b921273185a8e548796f
christiangubana <christiangubana@gmail.com>
2024-05-21 18:27:14 +0200

    distch out mongodb-in-memory databse

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,3 +1,21 @@
+// const { MongoMemoryServer } = require("mongodb-memory-server");
+// const mongoose = require("mongoose");
+
+// const connect = async () => {
+//   const mongoServer = await MongoMemoryServer.create();
+//   const mongoURL = mongoServer.getUri();
+
+//   const uri = process.env.MONGODB_URI || mongoURL
+//   mongoose.connect(mongoURL, {
+//     // useNewUrlParser: true,
+//     // useUnifiedTopology: true,
+//     dbName: "testingDb",
+//   });
+//   console.log(`MongoDB successfully connected to ${uri}`);
+// };
+
+// module.exports = connect;
+
 const mongoose = require("mongoose");
 
 const connect = async () => {
