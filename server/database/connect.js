f78c2ed43e075c8237ca9ecb415193f9a70868cb
christiangubana <christiangubana@gmail.com>
2024-05-21 18:51:01 +0200

    ensure the app run both on docker and on local machine

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,31 +1,18 @@
-// const { MongoMemoryServer } = require("mongodb-memory-server");
-// const mongoose = require("mongoose");
-
-// const connect = async () => {
-//   const mongoServer = await MongoMemoryServer.create();
-//   const mongoURL = mongoServer.getUri();
-
-//   const uri = process.env.MONGODB_URI || mongoURL
-//   mongoose.connect(mongoURL, {
-//     // useNewUrlParser: true,
-//     // useUnifiedTopology: true,
-//     dbName: "testingDb",
-//   });
-//   console.log(`MongoDB successfully connected to ${uri}`);
-// };
-
-// module.exports = connect;
-
 const mongoose = require("mongoose");
 
 const connect = async () => {
-  const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
-  mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "testingDb",
-  });
-  console.log(`MongoDB successfully connected to ${uri}`);
+  const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/testingDb";
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "testingDb",
+    });
+    console.log(`MongoDB successfully connected to ${uri}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
 };
 
 module.exports = connect;
