49e1ce90348ff7fc6b6018bebd83373a58ac37e1
christiangubana <christiangubana@gmail.com>
2024-05-24 00:23:02 +0200

    Able to run the application both locally and in a Docker environmen

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connect = async () => {
-  const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/testingDb";
+  let uri = process.env.MONGODB_URI;
+
+  // Check if we're running in a Docker container
+  if (!uri) {
+    if (process.env.NODE_ENV === "docker") {
+      uri = "mongodb://mongo:27017/testingDb";
+    } else {
+      uri = "mongodb://localhost:27017/testingDb";
+    }
+  }
+
   try {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
